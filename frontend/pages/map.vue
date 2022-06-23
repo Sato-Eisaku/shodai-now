506ae6c3a2a56ca368f0895a4a1324a945163b81
Sato-Eisaku <ouc0149eisa@gmail.com>
2022-06-24 02:03:24 +0900

    #4 マップの追加

--- frontend/pages/map.vue
+++ frontend/pages/map.vue
@@ -1,5 +1,18 @@
+<template>
+  <div>
+    <v-container class="white">
+      <MapImage />
+    </v-container>
+  </div>
+</template>
+
 <script>
+import MapImage from '@/assets/map.svg'
+
 export default {
-  name: 'MapPage'
+  name: 'MapPage',
+  components: {
+    MapImage
+  }
 }
 </script>
