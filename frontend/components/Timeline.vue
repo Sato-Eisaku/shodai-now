544adb8a1d432057fa394501f814e02a55a9b7ac
himantyu-yuma <lipton.happy.milktea@gmail.com>
2022-06-24 17:49:09 +0900

    #11 とりあえず表示ができるものを作った

---
+++ frontend/components/Timeline.vue
@@ -0,0 +1,13 @@
+<template>
+  <a class="twitter-timeline" href="https://twitter.com/k_y_kenzen/timelines/1539667358117433344?ref_src=twsrc%5Etfw">テスト1 - Curated tweets by k_y_kenzen</a>
+</template>
+<script>
+export default {
+  name: 'TimelineComponent',
+  mounted () {
+    const script = document.createElement('script')
+    script.src = 'https://platform.twitter.com/widgets.js'
+    document.body.appendChild(script)
+  }
+}
+</script>
